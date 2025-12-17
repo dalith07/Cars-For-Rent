adcada5c678a10b244e8c93ea6d08ae75972a804
kira <mohammedalitheiri@gmail.com>
2025-12-17 14:50:13 +0100

    routs commit

--- route.ts
+++ route.ts
@@ -4,7 +4,11 @@
  *  @type {string[]}
  */
 
-export const publicRoutes = ["/", "/auth/new-verification"];
+// export const publicRoutes = ["/", "/auth/new-verification"];
+export const publicRoutes = [
+  "https://cars-for-rent-h68n.vercel.app/",
+  "https://cars-for-rent-h68n.vercel.app//auth/new-verification",
+];
 
 /**
  *  An array of routes that are used for authentication
@@ -12,12 +16,20 @@ export const publicRoutes = ["/", "/auth/new-verification"];
  *  @type {string[]}
  */
 
+// export const authRoutes = [
+//   "/auth/login",
+//   "/auth/register",
+//   "/auth/error",
+//   "/auth/reset",
+//   "/auth/new-password",
+// ];
+
 export const authRoutes = [
-  "/auth/login",
-  "/auth/register",
-  "/auth/error",
-  "/auth/reset",
-  "/auth/new-password",
+  "https://cars-for-rent-h68n.vercel.app/api/auth/auth/login",
+  "https://cars-for-rent-h68n.vercel.app/api/auth/auth/register",
+  "https://cars-for-rent-h68n.vercel.app/api/auth/auth/error",
+  "https://cars-for-rent-h68n.vercel.app/api/auth/auth/reset",
+  "https://cars-for-rent-h68n.vercel.app/api/auth/auth/new-password",
 ];
 
 /**
@@ -26,11 +38,14 @@ export const authRoutes = [
  *  @type {string}
  */
 
-export const apiAuthPrefix = "/api/auth";
+// export const apiAuthPrefix = "/api/auth";
+export const apiAuthPrefix = "https://cars-for-rent-h68n.vercel.app/api/auth";
 
 /**
  *  The default redirect path after logging in
  *  @type {string}
  */
 
-export const DEFAULT_LOGIN_REDIRECT = "/";
+// export const DEFAULT_LOGIN_REDIRECT = "/";
+
+export const DEFAULT_LOGIN_REDIRECT = "https://cars-for-rent-h68n.vercel.app/";
